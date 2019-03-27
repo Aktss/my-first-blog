e48e87c3a4f9ab5d7a3037b3822b7a9dd0365065
Your name <testik@test.com>
2019-03-27 16:38:32 +0300

    Changes fro HTML for site

--- landing/urls.py
+++ landing/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from landing import views
+from django.urls import path, include
+
 
 
 urlpatterns = [
     url(r'^landing/', views.landing, name='landing')
+
 ]
